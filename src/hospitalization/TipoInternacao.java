d767cd2e5f49eafcae93d9e8c99b57bca8315cab
RodrigoCoouto <rodcoutocontas@gmail.com>
2022-10-18 12:36:43 -0300

    feat: Subindo internacao

---
+++ src/hospitalization/TipoInternacao.java
@@ -0,0 +1,13 @@
+package hospitalization;
+
+public class TipoInternacao {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
