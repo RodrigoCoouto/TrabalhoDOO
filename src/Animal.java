60f240846296742afec2f864e053092df09b0a25
RodrigoCoouto <rodcoutocontas@gmail.com>
2022-10-18 12:36:26 -0300

    feat: Subindo animal, alimentacao,raca, especie

---
+++ src/Animal.java
@@ -0,0 +1,39 @@
+public class Animal {
+
+    private String nome;
+    private float idade;
+    private float peso;
+    private float altura;
+    private boolean adotado;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getIdade() {
+        return idade;
+    }
+    public void setIdade(float idade) {
+        this.idade = idade;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    public float getAltura() {
+        return altura;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    public boolean isAdotado() {
+        return adotado;
+    }
+    public void setAdotado(boolean adotado) {
+        this.adotado = adotado;
+    }
+    
+}
