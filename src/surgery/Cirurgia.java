dc67c0d924eb10e9e1817f925bb86c5110a43330
RodrigoCoouto <rodcoutocontas@gmail.com>
2022-10-18 12:37:35 -0300

    feat: Subindo cirurgias

---
+++ src/surgery/Cirurgia.java
@@ -0,0 +1,17 @@
+package surgery;
+
+import java.sql.Date;
+
+public class Cirurgia {
+
+    private Date data;
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+}
---
+++ src/surgery/TipoCirurgia.java
@@ -0,0 +1,15 @@
+package surgery;
+
+public class TipoCirurgia {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
