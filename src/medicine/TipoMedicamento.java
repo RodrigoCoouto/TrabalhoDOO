7801aa714ffb546bbfe06f9b53c454fadbd0e0b8
RodrigoCoouto <rodcoutocontas@gmail.com>
2022-10-18 12:37:20 -0300

    feat: Subindo medicamentos

---
+++ src/medicine/TipoMedicamento.java
@@ -0,0 +1,15 @@
+package medicine;
+
+public class TipoMedicamento {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
