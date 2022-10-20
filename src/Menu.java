ccea8546c48ec5c6edbc11e141e509c05217e06c
João Pedro Garcia Pereira <garcia.jops@gmail.com>
2022-10-20 00:13:29 -0300

    molde pra criar opcao2

--- src/Menu.java
+++ src/Menu.java
@@ -40,7 +40,7 @@ public class Menu {
                 break;
 
             case 2:
-                
+                opcao2();
                 break;
 
             case 3:
@@ -126,4 +126,96 @@ public class Menu {
         }
     } 
     
+
+    protected static void opcao2() throws IOException, InterruptedException{
+        Scanner menu = new Scanner (System.in);
+        int opcao = 0;
+        //VARIAVEIS A SEREM UTILIZADAS NO MENU 
+
+        String nome;
+        int idade;
+        float peso;
+        float altura;
+        String especie;
+        String raca;
+
+
+        while (opcao!=5) {            
+            if (System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }
+            else{
+                Runtime.getRuntime().exec("clear");
+            }
+            System.out.print("##---------------------Menu-------------------##\n\n");
+            System.out.print("|------------------------------------------------|\n");
+            System.out.print("| Opção 1 - Criar Animal                         |\n");
+            System.out.print("| Opção 2 - Editar Animal                        |\n");
+            System.out.print("| Opção 3 - Remover Animal                       |\n");
+            System.out.print("| Opção 4 - Listar animais                       |\n");
+            System.out.print("| Opção 5 - Voltar                               |\n");
+            System.out.print("|------------------------------------------------|\n");
+            System.out.print("Digite uma opção: ");
+
+            opcao = menu.nextInt();
+
+             if (opcao == 4) {
+                 menu.close();
+             }
+
+            switch (opcao) {
+            case 1:
+                //Criar animal
+                System.out.println("Informe o NOME do Animal: "); 
+                nome=menu.nextLine();
+                //pede idade;
+                System.out.println("Informe a IDADE: ");
+                idade =menu.nextInt();
+                //pede peso;
+                System.out.println("Informe o PESO: ");
+                peso=menu.nextFloat();
+                //pede altura;
+                System.out.println("Informe a ALTURA: ");
+                altura=menu.nextFloat();
+                //pede especie;
+                System.out.println("Informe a ESPECIE: ");
+                especie=menu.nextLine(); 
+                //pede raca;
+                System.out.println("Informe a RACA: ");
+                raca=menu.nextLine();
+
+
+                break;
+
+            case 2:
+                System.out.println("Informe o nome do Animal: "); //DUVIDA, que atributo usar para buscar
+
+                //CHAMA METODO DE BUSCA
+
+                //CHAMA MENU DE OPCOES EDITAR ANIMAL
+
+                break;
+
+            case 3:
+                System.out.println("Informe o nome do Animal: "); //DUVIDA, que atributo usar para buscar
+
+                //CHAMA METODO DE BUSCA
+
+                // CHAMA METODO DE RECOMOCAO
+                
+                break;
+            case 4:
+                return;
+            default:
+                System.out.print("\nOpção Inválida!");
+
+                break;
+            }
+            if(opcao!=5){
+                System.out.print("\nPressione Enter para continuar...");
+                menu.nextLine();
+            }
+        }
+    }
+
 }
